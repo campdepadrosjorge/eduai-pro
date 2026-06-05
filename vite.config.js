1b18d0671d10b75543ed2dd8d1d0a7207a8edb7e
campdepadrosjorge <campdepadros.jorge@gmail.com>
2026-06-05 15:23:25 -0300

    fix: cambia nombre de bundle para forzar CDN cache bust

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/app-[hash].js",
+      }
+    }
+  }
 });
